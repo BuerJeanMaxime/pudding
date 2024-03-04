f0f8145a8a02279dd9571e92037d7c79b24c8018
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-04 11:55:34 +0100

    feat(Tokenizer): create main function to tokenize and get the weight of each relation

--- App/tokenizer/Tokenizer.py
+++ App/tokenizer/Tokenizer.py
@@ -1,3 +1,9 @@
+from itertools import combinations
+from typing import List
+
+from App.Token.Token import Token
+
+
 class Tokenizer:
     def __init__(self,syntagms,mwes,spacy,links_finder):
         self.syntagms = syntagms
@@ -6,5 +12,13 @@ class Tokenizer:
         self.links_finder = links_finder
 
     def tokenize(self):
-        pass
+        tokens = [Token(spacy_token) for spacy_token in self.spacy(" ".join(self.syntagms))]
+        self.find_relations_between_tokens(tokens)
+        return tokens
+    def find_relations_between_tokens(self, tokens: List):
+        for token_1,token_2 in combinations(tokens, r=2):
+            score = token_1.similarity(token_2)
+            if score < 0.55:
+                continue
+            token_1.relation[token_2] = score
 
