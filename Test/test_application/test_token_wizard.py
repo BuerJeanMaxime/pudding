ffd7021f11079d2808e65d3a4eed1f88818a4b18
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-04 15:44:41 +0100

    test(test_token_wizard): create a test to see if the tokenier does the right thing
    It does, now we have to use the way we do the relation

--- Test/test_application/test_token_wizard.py
+++ Test/test_application/test_token_wizard.py
@@ -1,11 +1,45 @@
 import unittest
 
+import spacy
+
+from App.tokenizer.Tokenizer import TokenWizard
+
 
 class TestTokenWizardWhithoutSpacy(unittest.TestCase):
 
     def helper_init_token_wizard__whithout_spacy(self):
         pass
+
+
 class TestTokenWizardWhithSpacy(unittest.TestCase):
 
     def helper_init_token_wizard__whith_spacy(self):
-        pass
+        syntagms = ["J'aime le foot", "Le football c'est bien", "Franchement le soccer c'est cool"]
+        spacy_lang = spacy.load("fr_core_news_md")
+        tw = TokenWizard(syntagms, [], spacy_lang)
+        return tw
+
+    def helper_assert_tokens_object(self, enchanced_tokens):
+        assertable_tokens = [token.spacy_token.text for token in enchanced_tokens]
+        return assertable_tokens
+
+    def test_enchance_tokens__with_spacy(self):
+        tw = self.helper_init_token_wizard__whith_spacy()
+        assertable_tokens = tw.enchance_tokens()
+        output = self.helper_assert_tokens_object(assertable_tokens)
+        expected = ["J'",
+                    'aime',
+                    'le',
+                    'foot',
+                    'Le',
+                    'football',
+                    "c'",
+                    'est',
+                    'bien',
+                    'Franchement',
+                    'le',
+                    'soccer',
+                    "c'",
+                    'est',
+                    'cool']
+        self.assertEqual(expected, output)
