7a0578ac6b4a2fb5377c2d77f8f0e671e160953c
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-04 16:06:08 +0100

    test(test_token_wizard): create a way to assert the relation of each token

--- Test/test_application/test_token_wizard.py
+++ Test/test_application/test_token_wizard.py
@@ -23,23 +23,44 @@ class TestTokenWizardWhithSpacy(unittest.TestCase):
         assertable_tokens = [token.spacy_token.text for token in enchanced_tokens]
         return assertable_tokens
 
+    def helper_assert_tokens_object__for_relations(self, enchanced_tokens):
+        assertable_relations = [{token.spacy_token.text: str(token.relation)} for token in enchanced_tokens]
+        return assertable_relations
+
     def test_enchance_tokens__with_spacy(self):
         tw = self.helper_init_token_wizard__whith_spacy()
-        assertable_tokens = tw.enchance_tokens()
-        output = self.helper_assert_tokens_object(assertable_tokens)
-        expected = ["J'",
-                    'aime',
-                    'le',
-                    'foot',
-                    'Le',
-                    'football',
-                    "c'",
-                    'est',
-                    'bien',
-                    'Franchement',
-                    'le',
-                    'soccer',
-                    "c'",
-                    'est',
-                    'cool']
-        self.assertEqual(expected, output)
+        output = tw.enchance_tokens()
+        assertable_tokens = self.helper_assert_tokens_object(output)
+        expected_token = ["J'",
+                          'aime',
+                          'le',
+                          'foot',
+                          'Le',
+                          'football',
+                          "c'",
+                          'est',
+                          'bien',
+                          'Franchement',
+                          'le',
+                          'soccer',
+                          "c'",
+                          'est',
+                          'cool']
+        expected_relations = [{"J'": '{}'},
+                              {'aime': '{}'},
+                              {'le': '{Le: 0.6559335589408875, le: 1.0}'},
+                              {'foot': '{football: 0.7234567403793335}'},
+                              {'Le': '{le: 0.6559335589408875}'},
+                              {'football': '{soccer: 0.6507284045219421}'},
+                              {"c'": "{c': 1.0}"},
+                              {'est': '{est: 1.0}'},
+                              {'bien': '{Franchement: 0.5703409314155579}'},
+                              {'Franchement': '{}'},
+                              {'le': '{}'},
+                              {'soccer': '{}'},
+                              {"c'": '{}'},
+                              {'est': '{}'},
+                              {'cool': '{}'}]
+        assertable_relations = self.helper_assert_tokens_object__for_relations(output)
+        self.assertEqual(expected_token, assertable_tokens)
+        self.assertEqual(expected_relations, assertable_relations)
