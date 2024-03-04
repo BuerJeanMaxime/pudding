1895d43b02787434fe83540b1626ef52d0ffc987
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-04 12:09:37 +0100

    wip(test_token_wizard): just created empty classes

---
+++ Test/test_application/test_token_wizard.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestTokenWizardWhithoutSpacy(unittest.TestCase):
+
+    def helper_init_token_wizard__whithout_spacy(self):
+        pass
+class TestTokenWizardWhithSpacy(unittest.TestCase):
+
+    def helper_init_token_wizard__whith_spacy(self):
+        pass
