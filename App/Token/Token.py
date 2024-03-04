69e2847df50d6dfbb90f1c8fff3e5727a4205292
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-04 15:43:55 +0100

    chore(Token): __repr__ to facilitate the debug sessiosn

--- App/Token/Token.py
+++ App/Token/Token.py
@@ -4,5 +4,8 @@ from typing import Dict
 class Token:
 
     def __init__(self,spacy_token):
-        self.spacy_token: str = spacy_token
+        self.spacy_token = spacy_token
         self.relation: Dict[Token,float] = {}
+
+    def __repr__(self):
+        return f"{self.spacy_token.text}"
