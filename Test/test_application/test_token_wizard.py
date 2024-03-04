d1bedf417543f61f12da03c3eb6734aa0deb1faf
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-04 16:09:07 +0100

    chore(test_token_wizard): deleting useless tests

--- Test/test_application/test_token_wizard.py
+++ Test/test_application/test_token_wizard.py
@@ -4,13 +4,6 @@ import spacy
 
 from App.tokenizer.Tokenizer import TokenWizard
 
-
-class TestTokenWizardWhithoutSpacy(unittest.TestCase):
-
-    def helper_init_token_wizard__whithout_spacy(self):
-        pass
-
-
 class TestTokenWizardWhithSpacy(unittest.TestCase):
 
     def helper_init_token_wizard__whith_spacy(self):
