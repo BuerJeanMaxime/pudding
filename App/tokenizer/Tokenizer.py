1bdb745d2d93b8596babbbbd4810b5b616a8c3aa
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-04 11:40:21 +0100

    feat(Tokenizer): create the main skeltton

---
+++ App/tokenizer/Tokenizer.py
@@ -0,0 +1,10 @@
+class Tokenizer:
+    def __init__(self,syntagms,mwes,spacy,links_finder):
+        self.syntagms = syntagms
+        self.mwes = mwes
+        self.spacy = spacy
+        self.links_finder = links_finder
+
+    def tokenize(self):
+        pass
+
