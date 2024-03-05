19c3bd06eb108e3592e707021f33950294c9f0cb
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-05 09:27:47 +0100

    feat(GraphBuilder): create the Class and function to create the graph

---
+++ App/graph_builder/GraphBuilder.py
@@ -0,0 +1,21 @@
+import networkx as nx
+
+
+class GraphBuilder:
+    def __init__(self,enchanced_tokens):
+        self.enchanced_tokens = enchanced_tokens
+        self.graph = nx.Graph()
+
+    def get_complete_graph(self):
+        self._constitute_graph()
+        return self.graph
+    def _add_nodes(self):
+        self.graph.add_nodes_from(self.enchanced_tokens)
+    def _add_edges_and_weight(self):
+        for token in self.enchanced_tokens:
+            for related_token, weight in token.relation.items():
+                self.graph.add_edge(token,related_token,weight=weight)
+
+    def _constitute_graph(self):
+        self._add_nodes()
+        self._add_edges_and_weight()
