4ae103085f4b5dbd9a09b4d983ed7204c170ae9a
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-07 11:03:41 +0100

    test(test_community_center): new test
    The test is to make that community finder works perfectly

---
+++ Test/test_application/test_community_center.py
@@ -0,0 +1,29 @@
+import unittest
+
+from App.Token.Token import Token
+from App.communities_finder.CommunityCenter import CommunityCenter
+from App.graph_builder.GraphBuilder import GraphBuilder
+from Test.mocks.MockSpacyToken import MockSpacyToken
+
+
+class TestCommunityCenter(unittest.TestCase):
+
+    def helper_create_token_list(self):
+        foot = Token(MockSpacyToken("foot"))
+        gf38 = Token(MockSpacyToken("gf38"))
+
+        foot.relation[gf38] = 900
+        gf38.relation[foot] = 878
+        enchanced_tokens = [foot, gf38]
+        return enchanced_tokens
+
+    def helper_create_community_center_object(self):
+        tokens = self.helper_create_token_list()
+        graph = GraphBuilder(enchanced_tokens=tokens).get_complete_graph()
+        cc = CommunityCenter(graph=graph)
+        return cc
+
+    def test_get_communities(self):
+        output = self.helper_create_community_center_object().get_communities()
+        expected = [['foot', 'gf38']]
+        self.assertEqual(expected,output)
