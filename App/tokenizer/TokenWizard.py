0270aa22e00e8da68966dc724944e7cc99b27bf9
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-07 14:52:56 +0100

    feat(TokenWizard): add a new parameter to know wich token is relevent

--- App/tokenizer/TokenWizard.py
+++ App/tokenizer/TokenWizard.py
@@ -13,13 +13,23 @@ class TokenWizard:
         self.spacy = spacy.load(spacy_name)
 
     def enchance_tokens(self):
-        tokens = [Token(spacy_token) for spacy_token in self.spacy(" ".join(self.syntagms))]
+        tokens = [Token(spacy_token,self.is_relevent_token(spacy_token)) for spacy_token in self.spacy(" ".join(self.syntagms))]
         self.find_relations_between_tokens(tokens)
         return tokens
     def find_relations_between_tokens(self, tokens: List):
         for token_1,token_2 in permutations(tokens, r=2):
+            if not token_1.is_relevent or not token_2.is_relevent:
+                continue
             score = token_1.spacy_token.similarity(token_2.spacy_token)
             if score < 0.55:
                 continue
             token_1.relation[token_2] = (score*1000)
 
+    def is_relevent_token(self,spacy_token):
+        return not ((spacy_token.is_punct)
+            or (spacy_token.is_stop)
+            or (spacy_token.is_bracket)
+            or (spacy_token.is_quote)
+            or (spacy_token.is_oov)
+            or (spacy_token.pos_ == "DET")
+            or (spacy_token.pos_ == "PRON"))
