e6f6e683deab10f5e1129ab4f2ebbd54e67521fd
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-07 12:15:48 +0100

    fix(Application): put the correct methods names

--- App/Application.py
+++ App/Application.py
@@ -3,13 +3,13 @@ from App.tokenizer.TokenWizard import TokenWizard
 
 class Application:
 
-    def __init__(self,token_wizard,graph_builder,communities_finder):
+    def __init__(self,token_wizard,graph_builder,community_center):
         self.token_wizard: TokenWizard = token_wizard
         self.graph_builder = graph_builder
-        self.communities_finder = communities_finder
+        self.community_center = community_center
 
     def run(self):
         enchanced_tokens = self.token_wizard.enchance_tokens()
-        graph = self.graph_builder.create_graph(enchanced_tokens)
-        communities = self.communities_finder.extract_communities(graph)
+        graph = self.graph_builder.get_complete_graph()
+        communities = self.community_center.get_communities()
         return communities
