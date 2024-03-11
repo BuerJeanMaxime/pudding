ef6bd70c100de1022128757883e355fd35c15030
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-11 10:42:13 +0100

    chore(): remove useless varaibles and add typing

--- App/Token/Token.py
+++ App/Token/Token.py
@@ -6,7 +6,7 @@ class Token:
     def __init__(self,spacy_token,is_relevent: bool):
         self.spacy_token = spacy_token
         self.relation: Dict[Token,float] = {}
-        self.is_relevent = is_relevent
+        self.is_relevent: bool = is_relevent
 
     def __repr__(self):
         return f"{self.spacy_token.text}"
