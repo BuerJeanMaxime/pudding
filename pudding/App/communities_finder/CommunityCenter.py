f180f7c725c550a2ff91337db9bccb78dbb61fad
buerj <jeanmaxime.buer.pro@gmail.com>
2024-03-21 10:02:19 +0100

    refactor(CommunityCenter): using cd lib instead of louvain
    We want to use cdlib to be able, later, to make F1 test

--- pudding/App/communities_finder/CommunityCenter.py
+++ pudding/App/communities_finder/CommunityCenter.py
@@ -1,9 +1,7 @@
-from typing import Dict, List
+from typing import List
 
-import community as community_louvain
 import networkx as nx
-
-from pudding.App.Token.Token import Token
+from cdlib import algorithms
 
 
 class CommunityCenter:
@@ -11,12 +9,5 @@ class CommunityCenter:
         self.graph: nx.Graph = graph
 
     def get_communities(self):
-        communities: List[List[str]] = self._translate_at_the_right_format(community_louvain.best_partition(self.graph,randomize=False,weight='weight'))
+        communities: List[List[str]] = [[el.spacy_token.text for el in first_list] for first_list in algorithms.louvain(self.graph, weight='weight').communities]
         return communities
-
-    def _translate_at_the_right_format(self, community_wrong_format : Dict[Token,int]) -> List[List[str]]:
-        nb_communities: int = max(community_wrong_format.values())
-        community_right_format: List[List[str]] = [[] for _ in range(nb_communities+1)]
-        for node,nb_community in community_wrong_format.items():
-            community_right_format[nb_community].append(node.spacy_token.text)
-        return community_right_format
