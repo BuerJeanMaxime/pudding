08608be9c5df6d6f2f784660ca454d5b4e4e8c13
buerjhp2 <jean-maxime.buer@univ-grenoble-alpes.fr>
2024-03-25 09:59:56 +0100

    fix(TokenWizard): new way to calculate the weight
    we made a few changes about wich parameters we took, and about how the weight is computed.

--- pudding/App/tokenizer/TokenWizard.py
+++ pudding/App/tokenizer/TokenWizard.py
@@ -1,5 +1,6 @@
+import math
 from itertools import permutations
-from typing import List
+from typing import List, Union
 
 import spacy
 
@@ -21,12 +22,23 @@ class TokenWizard:
             if not token_1.is_relevent or not token_2.is_relevent:
                 continue
             score = token_1.spacy_token.similarity(token_2.spacy_token)
-            if score < 0.55:
+            if score < 0.45:
                 continue
-            token_1.relation[token_2] = (score*1000 * self.is_same_tokens(token_1,token_2))
+            token_1.relation[token_2] = (
+                        math.log2(
+                            (score*1000 * self.is_same_tokens(token_1,token_2)) *
+                            self.is_identical_tokens(token_1, token_2)
+                        )
+            )
 
-    def is_same_tokens(self,token_1,token_2) -> int:
-        return 0.8 if token_1.spacy_token.lemma_ == token_2.spacy_token.lemma_ else 1
+    def is_same_tokens(self,token_1,token_2) -> Union[int,float]:
+        return 0.5 if token_1.spacy_token.lemma_ == token_2.spacy_token.lemma_ else 1
+
+    def is_same_pos(self, token_1, token_2) -> Union[int,float]:
+        return 0.5 if token_1.spacy_token.pos_ == token_2.spacy_token.pos_ else 1
+
+    def is_identical_tokens(self,token_1, token2) -> Union[int,float]:
+        return 0.5 if token_1.spacy_token.text == token2.spacy_token.text else 1
 
     def is_relevent_token(self,spacy_token) -> bool:
         return not ((spacy_token.is_punct)
